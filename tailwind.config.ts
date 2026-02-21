4b8164f0f8f610ae6660db82bebe551cb67d0384
Lseojeong <sjung0314@gachon.ac.kr>
2026-02-22 01:53:55 +0900

    ✨[feat]: pxr 도입

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'tailwindcss';
+import pxToRemPreset from 'tailwindcss-preset-px-to-rem';
+
+const config: Config = {
+  presets: [pxToRemPreset],
+};
+
+export default config;
