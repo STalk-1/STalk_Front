11007cad505adf1a91b8d2e099a1fb2f56239b0c
Lseojeong <sjung0314@gachon.ac.kr>
2026-02-28 21:59:01 +0900

    ✨[feat]: 아이콘 및 이미지 세팅

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,33 @@
+import IcArrowDown from './ic-arrow-down.svg?react';
+import IcArrowUp from './ic-arrow-up.svg?react';
+import IcBot from './ic-bot.svg?react';
+import IcCaretDown from './ic-caret-down.svg?react';
+import IcCaretUp from './ic-caret-up.svg?react';
+import IcChat from './ic-chat.svg?react';
+import IcGraph from './ic-graph.svg?react';
+import IcKospi from './ic-kospi.svg?react';
+import IcLike from './ic-like.svg?react';
+import IcNasdaq from './ic-nasdaq.svg?react';
+import IcSandP from './ic-s&p.svg?react';
+import IcSearch from './ic-search.svg?react';
+import IcSend from './ic-send.svg?react';
+import IcSideMenu from './ic-side-menu.svg?react';
+import IcUsd from './ic-usd.svg?react';
+
+export {
+  IcArrowDown,
+  IcArrowUp,
+  IcBot,
+  IcCaretDown,
+  IcCaretUp,
+  IcChat,
+  IcGraph,
+  IcKospi,
+  IcLike,
+  IcNasdaq,
+  IcSandP,
+  IcSearch,
+  IcSend,
+  IcSideMenu,
+  IcUsd,
+};
