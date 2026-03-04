65f4539a5cd54ca7dc9a2497862ffb012b0fa780
Lseojeong <sjung0314@gachon.ac.kr>
2026-03-04 19:20:02 +0900

    🔥 [remove]: px to rem 제거(Tailwind CSS InteliSense 확장 기능 사용으로 삭제)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from 'tailwindcss';
-import pxToRemPreset from 'tailwindcss-preset-px-to-rem';
 
-const config: Config = {
-  presets: [pxToRemPreset],
-};
+const config: Config = {};
 
 export default config;
--- tailwindcss-preset-px-to-rem.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'tailwindcss-preset-px-to-rem' {
-  import type { Config } from 'tailwindcss'
-
-  const pxToRemPreset: Config
-
-  export default pxToRemPreset
-}
