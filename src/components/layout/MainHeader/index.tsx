8dffb9bc3a451d913caaacf6f55f44679742e089
Lseojeong <sjung0314@gachon.ac.kr>
2026-03-05 18:43:47 +0900

    💄[design]: 헤더 구조 변경

--- src/components/layout/MainHeader/index.tsx
+++ src/components/layout/MainHeader/index.tsx
@@ -1,27 +1,22 @@
+import { Link } from 'react-router-dom';
+
 import { Logo } from '@/assets/images';
 
-import { HEADER_NAV_TEXTS } from './constants';
 import LoginButton from './LoginButton';
 
 function Header() {
   return (
-    <header className="border-bg h-20 w-full border-b bg-white">
-      <nav className="mx-auto flex h-full w-full max-w-300 items-center gap-10 px-5">
+    <header className="shadow-xl h-20 w-full bg-white">
+      <nav className="layout-content flex h-full w-full items-center px-5">
         <h1>
-          <Logo
-            className="md:h-9 md:w-19 lg:h-9 lg:w-19"
-            width={51}
-            height={24}
-          />
+          <Link to="/" aria-label="홈으로 이동">
+            <Logo
+              className="md:h-9 md:w-19 lg:h-9 lg:w-19"
+              width={51}
+              height={24}
+            />
+          </Link>
         </h1>
-        <ul className="flex h-full items-center gap-6">
-          <li className="typo-12-medium md:typo-20-medium lg:typo-20-medium relative flex h-full items-center text-green-500 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-green-500 after:content-['']">
-            {HEADER_NAV_TEXTS.REAL_TIME_CHART}
-          </li>
-          <li className="typo-12-medium text-grey-500 md:typo-20-medium lg:typo-20-medium">
-            {HEADER_NAV_TEXTS.INTEREST_STOCKS}
-          </li>
-        </ul>
         <LoginButton />
       </nav>
     </header>
