f68160a17b632e0a2e3e01700ed4ad9f685caadc
Lseojeong <sjung0314@gachon.ac.kr>
2026-03-06 16:32:54 +0900

    🚚[rename]: S&P500 아이콘 이름 변경

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -8,7 +8,7 @@ import IcGraph from './ic-graph.svg?react';
 import IcKospi from './ic-kospi.svg?react';
 import IcLike from './ic-like.svg?react';
 import IcNasdaq from './ic-nasdaq.svg?react';
-import IcSandP from './ic-s&p.svg?react';
+import IcSNP from './ic-s&p.svg?react';
 import IcSearch from './ic-search.svg?react';
 import IcSend from './ic-send.svg?react';
 import IcSideMenu from './ic-side-menu.svg?react';
@@ -25,9 +25,9 @@ export {
   IcKospi,
   IcLike,
   IcNasdaq,
-  IcSandP,
   IcSearch,
   IcSend,
   IcSideMenu,
+  IcSNP,
   IcUsd,
 };
