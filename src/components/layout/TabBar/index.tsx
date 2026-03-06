9082c60567acc125b685db69d0525ac75564315a
Lseojeong <sjung0314@gachon.ac.kr>
2026-03-06 15:09:15 +0900

    ♻️[refactor]: TabBar 중복 제거(map으로 변경)

--- src/components/layout/TabBar/index.tsx
+++ src/components/layout/TabBar/index.tsx
@@ -1,39 +1,25 @@
 import { NavLink } from 'react-router-dom';
 
-import { TAB_BAR_ITEMS } from './constants';
+import { TAB_BAR_ITEMS, TAB_CLASS_NAMES } from './constants';
 
 function TabBar() {
   return (
     <ul className="flex h-full items-center gap-10">
-      <li className="flex h-full items-center">
-        <NavLink
-          to={TAB_BAR_ITEMS.REAL_TIME_CHART.path}
-          end
-          className={({ isActive }) =>
-            `typo-12-medium md:typo-20-medium lg:typo-20-medium relative flex h-16 items-center ${
-              isActive
-                ? "text-green-500 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-green-500 after:content-['']"
-                : 'text-grey-500'
-            }`
-          }
-        >
-          {TAB_BAR_ITEMS.REAL_TIME_CHART.label}
-        </NavLink>
-      </li>
-      <li className="flex h-full items-center">
-        <NavLink
-          to={TAB_BAR_ITEMS.INTEREST_STOCKS.path}
-          className={({ isActive }) =>
-            `typo-12-medium md:typo-20-medium lg:typo-20-medium relative flex h-16 items-center ${
-              isActive
-                ? "text-green-500 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-green-500 after:content-['']"
-                : 'text-grey-500'
-            }`
-          }
-        >
-          {TAB_BAR_ITEMS.INTEREST_STOCKS.label}
-        </NavLink>
-      </li>
+      {Object.values(TAB_BAR_ITEMS).map((item) => (
+        <li key={item.path} className="flex h-full items-center">
+          <NavLink
+            to={item.path}
+            end={item.end}
+            className={({ isActive }) =>
+              `${TAB_CLASS_NAMES.base} ${
+                isActive ? TAB_CLASS_NAMES.active : TAB_CLASS_NAMES.inactive
+              }`
+            }
+          >
+            {item.label}
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 }
